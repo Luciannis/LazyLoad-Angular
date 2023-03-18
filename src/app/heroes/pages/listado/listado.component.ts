3df6d86eeb97a5c53beae08b7aa82093f7b58185
Francisco Riveros <francisco@Franciscos-MacBook-Air.local>
2023-03-17 21:08:57 -0300

    retomando modulo heroes

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styles: [`
+  mat-card {
+    margin-top: 20px
+  }`]
 })
 export class ListadoComponent {
-
+  heroes: Heroe[] = [];
+  constructor(private heroesService:HeroesService){}
+    ngOnInit(): void {
+      this.heroesService.getHores()
+      .subscribe( r => {
+        this.heroes = r;
+      })
+    }
+  
 }
